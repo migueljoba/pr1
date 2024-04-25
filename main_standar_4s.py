0eea3f8c9e64cec4452d23fe24b9af7d02f15d30
Miguel Baez <mjose.baez@gmail.com>
2024-04-24 23:26:19 -0400

    Renderizado de mapa de 4 estados "4s" - Agrega archivos para probar caldeidoscopio de 2 y 4 colores

---
+++ main_standar_4s.py
@@ -0,0 +1,62 @@
+import time
+
+import numpy as np
+
+import utils
+import utils_file
+import utils_plot
+
+file_dir = "./data_source/"
+filename = "kaleido_4s"
+rule = utils.Rule()
+rule.b = 1.85
+rule.matrix = [
+    [0, 0, rule.b, rule.b],
+    [0, 0, rule.b, rule.b],
+    [0, 0, 1, 1],
+    [0, 0, 1, 1]
+]
+# rotator con b = 1.67 o mayor
+
+transition_map = [
+    [0, 0, 2, 2],
+    [0, 0, 2, 2],
+    [1, 1, 3, 3],
+    [1, 1, 3, 3]
+]
+
+filepath = file_dir + filename + ".csv"
+
+print(f"Opening {filepath}")
+
+population_array = utils_file.import_csv(filepath)
+initial_population = np.array(population_array, dtype=int)
+
+matrix_list = [initial_population]
+
+for step in range(50):
+    print(f"Step {step}")
+
+    current_step = np.zeros(initial_population.shape, dtype=int)
+    previous_step = matrix_list[-1]
+
+    payoff_array = utils.generate_weight_array(previous_step, rule)
+
+    for idx_i, idx_j in np.ndindex(payoff_array.shape):
+        neighbours_payoff = utils.get_neighbours(payoff_array, idx_i, idx_j)
+        winner_idx = utils.get_highest_element_idx(neighbours_payoff)
+        neighbours = utils.get_neighbours(previous_step, idx_i, idx_j)
+
+        invader = neighbours[winner_idx[0]][winner_idx[1]]
+        current = previous_step[idx_i][idx_j]
+
+        result = transition_map[current][invader]
+
+        current_step[idx_i, idx_j] = result
+
+    matrix_list.append(current_step)
+
+for idx, m in enumerate(matrix_list):
+    print(f"Plotting: {idx}")
+    time.sleep(0.7)
+    utils_plot.plot_4_color_array(m, step=idx, b=rule.b, file_prefix=filename)
--- utils_plot.py
+++ utils_plot.py
@@ -3,7 +3,7 @@ import matplotlib.colors as colors
 import numpy as np
 
 
-def plot_binary_array(array, step=None, b=None, file_prefix=None, grid_data=False):
+def plot_binary_array(array, step=None, b=None, title: str = None, file_prefix=None, grid_data=False):
     color_defector = 'lightcoral'
     color_cooperator = 'steelblue'
     color_map = colors.ListedColormap([color_defector, color_cooperator])
@@ -17,7 +17,49 @@ def plot_binary_array(array, step=None, b=None, file_prefix=None, grid_data=Fals
 
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 
-    plt.title(f"Step: {step} - b: {b}")
+    if title is not None:
+        plt.title(title)
+    else:
+        plt.title(f"Step: {step} - b: {b}")
+
+    if grid_data:
+        for i, j in np.ndindex(array.shape):
+            ax.text(j, i, array[i, j],
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    color='white')
+
+    # TODO directorio de salida debe ser parametrizable
+    if file_prefix is not None:
+        filename = f"images/{file_prefix}-{b}-{step}.svg"
+    else:
+        filename = f"images/{b}-{step}.svg"
+
+    plt.savefig(filename, format="svg")
+    plt.close()
+
+
+def plot_4_color_array(array, step=None, b=None, title: str = None, file_prefix=None, grid_data=False):
+    color_defector = 'red'
+    color_new_defector = 'yellow'
+    color_new_cooperator = 'green'
+    color_cooperator = 'blue'
+
+    color_map = colors.ListedColormap([color_defector, color_new_defector, color_new_cooperator, color_cooperator])
+
+    fig, ax = plt.subplots(dpi=700)
+    ax.imshow(array, cmap=color_map)
+
+    total_rows, total_cols = array.shape
+    ax.set_xticks(np.arange(total_cols))
+    ax.set_yticks(np.arange(total_rows))
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    if title is not None:
+        plt.title(title)
+    else:
+        plt.title(f"Step: {step} - b: {b}")
 
     if grid_data:
         for i, j in np.ndindex(array.shape):
