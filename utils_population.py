ec0673ed36875a89d07cbbfe95021bf8362ffcc2
Miguel Baez <mjose.baez@gmail.com>
2024-05-03 21:38:38 -0400

    Se permite crear una población cuadrada NxN, donde el valor de N se define por parámetro 'sides'. - tiene prioridad sobre parámetros de fila y columna

--- utils_population.py
+++ utils_population.py
@@ -4,13 +4,17 @@ DEFECTOR: int = 0
 COOPERATOR: int = 1
 
 
-def single_defector(rows: int = 20, cols: int = 20) -> np.ndarray:
+def single_defector(rows: int = 20, cols: int = 20, sides: int = None) -> np.ndarray:
     """
     Genera poblacion con un unico defector ubicado en el centro de filas y columnas
     :param rows: total de filas para la poblacion
     :param cols: total de columnas para la poblacion
+    :param sides: total de filas y total de columnas para la poblacion. Sobreescribe valores de rows y cols
     :return: ndarray de poblacion
     """
-    matrix = np.full(shape=(rows, cols), fill_value=COOPERATOR, dtype=np.int8)
-    matrix[int(rows / 2), int(cols / 2)] = DEFECTOR
+
+    dim = (sides, sides) if sides is not None else (rows, cols)
+
+    matrix = np.full(shape=(dim[0], dim[1]), fill_value=COOPERATOR, dtype=np.int8)
+    matrix[int(dim[0] / 2), int(dim[1] / 2)] = DEFECTOR
     return matrix
