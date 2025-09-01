efff4a53358a9815a0d4c367ec0ce9b325a48717
Miguel Baez <mjose.baez@gmail.com>
2025-09-01 13:59:25 -0300

    Utilitario para población con un solo cooperador

--- utils_population.py
+++ utils_population.py
@@ -13,6 +13,20 @@ def custom():
     ]
     return np.array(p)
 
+def single_cooperator(rows: int = 20, cols: int = 20, sides: int = None) -> np.ndarray:
+    """
+    Genera poblacion con un unico cooperador ubicado en el centro de filas y columnas
+    :param rows: total de filas para la poblacion
+    :param cols: total de columnas para la poblacion
+    :param sides: total de filas y total de columnas para la poblacion. Sobreescribe valores de rows y cols
+    :return: ndarray de poblacion
+    """
+
+    dim = (sides, sides) if sides is not None else (rows, cols)
+
+    matrix = np.full(shape=(dim[0], dim[1]), fill_value=DEFECTOR, dtype=np.int8)
+    matrix[int(dim[0] / 2), int(dim[1] / 2)] = COOPERATOR
+    return matrix
 
 def single_defector(rows: int = 20, cols: int = 20, sides: int = None) -> np.ndarray:
     """
